86d26efaf4df7242db3ceb3bedf3677deddcd6d0
Iván Zaera Avellón <izaera@gmx.es>
2021-03-19 12:25:49 +0100

    docs: document kern package

--- src/js/kern/index.js
+++ src/js/kern/index.js
@@ -1,12 +1,34 @@
+/** 
+ * @exports kern
+ */
 const kern = {};
 
-/** Low level print function */
+/** 
+ * Low level print function: prints the message to stderr without trailing '\n'. 
+ *
+ * This function shouldn't be used unless from very low level modules where `io`
+ * or `term` packages are not accessible.
+ *
+ * @param {string} message
+ * @return {void}
+ * @see {module:io#write_str}
+ * @see {module:term#print2}
+ */
 kern.printk = j.printk;
 
-/** Array of dirs to search for modules */
+/** 
+ * Array of dirs to search for modules
+ *
+ * @type {string[]}
+ */
 kern.search_path = [];
 
-/** Joshi interpreter version number */
+/** 
+ * Joshi interpreter version number: a string with three number separated by 
+ * periods (example: `1.0.2`)
+ *
+ * @type {string}
+ */
 kern.version = j.version;
 
 return kern;
