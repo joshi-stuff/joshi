27a43f5e906cc63f2a7752726ad90941a457e864
Iván Zaera Avellón <izaera@gmx.es>
2021-03-21 17:45:55 +0100

    docs: perf package

--- src/js/perf/index.js
+++ src/js/perf/index.js
@@ -1,9 +1,12 @@
-const term = require('term');
-
-const println = term.println;
-
+/**
+ * @exports perf
+ */
 const perf = {};
 
+/**
+ * @class
+ * @hideconstructor
+ */
 function PerfData(label) {
 	this.labels = [label];
 	this.samples = [performance.now()];
@@ -11,17 +14,35 @@ function PerfData(label) {
 
 PerfData.prototype = {
 
+	/**
+	 * Take an intermediate measure of elapsed time
+	 *
+	 * @param {string} label A label describing the measure
+	 * @returns {void}
+	 */
 	lap: function(label) {
 		this.labels.push(label);
 		this.samples.push(performance.now());
 	},
 
+	/**
+	 * Finish taking measures
+	 *
+	 * @returns {PerfData} The same object for which it has been called
+	 */
 	end: function() {
 		this.labels.push('end');
 		this.samples.push(performance.now());
 		return this;
 	},
 
+	/**
+	 * Return the report of taken measures
+	 *
+	 * @returns {string} 
+	 * A human readable string containing the elapsed and total times described
+	 * by their labels.
+	 */
 	report: function() {
 		var str = 'PERFORMANCE REPORT: ' + this.labels[0];
 
@@ -36,6 +57,12 @@ PerfData.prototype = {
 	},
 }
 
+/**
+ * Start a performance data collection with a resolution of milliseconds.
+ *
+ * @param {string} label The name of the data collection
+ * @returns {PerfData} An object with methods to perform the collection
+ */
 perf.start = function(label) {
 	return new PerfData(label);
 }
