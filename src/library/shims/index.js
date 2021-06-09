dbbb779fe93d427b6f5f9b9a7e352797dd0c483c
Iván Zaera Avellón <izaera@gmx.es>
2021-06-09 17:12:57 +0200

    feat(shims): add String.trimRight/end and String.trimLeft/Start

--- src/library/shims/index.js
+++ src/library/shims/index.js
@@ -103,3 +103,17 @@ String.prototype.endsWith = function (suffix) {
 String.prototype.startsWith = function (prefix) {
 	return this.indexOf(prefix) === 0;
 };
+
+String.prototype.trimLeft = String.prototype.trimStart = function () {
+	return this.replace(
+		/^[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]*/,
+		''
+	);
+};
+
+String.prototype.trimRight = String.prototype.trimEnd = function () {
+	return this.replace(
+		/[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]*$/,
+		''
+	);
+};
