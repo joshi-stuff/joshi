30d22a0b6d9730c522d24f46836c4e1e672f843e
Iván Zaera Avellón <izaera@gmx.es>
2021-10-07 14:21:57 +0200

    fix(dbus): support UTF-8 strings properly

--- src/joshi/joshi_dbus.c
+++ src/joshi/joshi_dbus.c
@@ -41,14 +41,14 @@ static DBusMessage* duk_get_DBusMessage_pt(duk_context* ctx, duk_idx_t idx) {
 }
 
 /* BEGIN CUSTOM USER CODE */
-#define log_push(...) /*fprintf(stderr, __VA_ARGS__)*/
-#define log_get(...) /*fprintf(stderr, __VA_ARGS__)*/
+#define log_push(...) //fprintf(stderr, __VA_ARGS__)
+#define log_get(...) //fprintf(stderr, __VA_ARGS__)
 
 // Helpers
 static const char* duk_get_field(duk_context* ctx, int idx, const char* field) {
 	duk_get_prop_string(ctx, idx, field);
 
-	const char* type = duk_get_string(ctx, -1);
+	const char* type = duk_get_char_pt(ctx, -1);
 
 	duk_pop(ctx); 
 
@@ -167,7 +167,7 @@ static void duk_push_dbus_variant(duk_context* ctx, DBusMessageIter* iter);
 static void duk_push_dbus_any(duk_context* ctx, DBusMessageIter* iter) {
 	char type = dbus_message_iter_get_arg_type(iter);
 
-	log_push(stderr, "[%p] push_any %c\n", iter, type);
+	log_push("[%p] push_any %c\n", iter, type);
 
 	switch (type) {
 		case DBUS_TYPE_ARRAY:      duk_push_dbus_array(ctx, iter); break;
@@ -185,12 +185,12 @@ static void duk_push_dbus_array(duk_context* ctx, DBusMessageIter* iter) {
 	duk_idx_t i = 0;
 
 	duk_push_array(ctx);
-	log_push(stderr, "[%p] pushed array\n", &sub_iter);
+	log_push("[%p] pushed array\n", &sub_iter);
 
 	do {
 		duk_push_dbus_any(ctx, &sub_iter);
 		duk_put_prop_index(ctx, -2, i);
-		log_push(stderr, "[%p] set [%d]\n", &sub_iter, i);
+		log_push("[%p] set [%d]\n", &sub_iter, i);
 		i++;
 	}
 	while (dbus_message_iter_next(&sub_iter));
@@ -199,7 +199,7 @@ static void duk_push_dbus_array(duk_context* ctx, DBusMessageIter* iter) {
 static void duk_push_dbus_basic(duk_context* ctx, DBusMessageIter* iter) {
 	char type = dbus_message_iter_get_arg_type(iter);
 
-	log_push(stderr, "[%p] push_basic %c\n", iter, type);
+	log_push("[%p] push_basic %c\n", iter, type);
 
 	DBusBasicValue value;
 	dbus_message_iter_get_basic(iter, &value);
@@ -233,17 +233,17 @@ static void duk_push_dbus_dict_entry(duk_context* ctx, DBusMessageIter* iter) {
 	duk_push_object(ctx);
 	duk_push_string(ctx, "DICT_ENTRY");
 	duk_put_prop_string(ctx, -2, "is_a");
-	log_push(stderr, "[%p] pushed object\n", &sub_iter);
+	log_push("[%p] pushed object\n", &sub_iter);
 
 	duk_push_dbus_any(ctx, &sub_iter);
 	duk_put_prop_string(ctx, -2, "key");
-	log_push(stderr, "[%p] set 'key'\n", &sub_iter);
+	log_push("[%p] set 'key'\n", &sub_iter);
 
 	dbus_message_iter_next(&sub_iter);
 
 	duk_push_dbus_any(ctx, &sub_iter);
 	duk_put_prop_string(ctx, -2, "value");
-	log_push(stderr, "[%p] set 'value'\n", &sub_iter);
+	log_push("[%p] set 'value'\n", &sub_iter);
 }
 
 static void duk_push_dbus_struct(duk_context* ctx, DBusMessageIter* iter) {
@@ -253,12 +253,12 @@ static void duk_push_dbus_struct(duk_context* ctx, DBusMessageIter* iter) {
 	duk_idx_t i = 0;
 
 	duk_push_array(ctx);
-	log_push(stderr, "[%p] pushed array\n", &sub_iter);
+	log_push("[%p] pushed array\n", &sub_iter);
 
 	do {
 		duk_push_dbus_any(ctx, &sub_iter);
 		duk_put_prop_index(ctx, -2, i);
-		log_push(stderr, "[%p] set [%d]\n", &sub_iter, i);
+		log_push("[%p] set [%d]\n", &sub_iter, i);
 		i++;
 	}
 	while (dbus_message_iter_next(&sub_iter));
@@ -283,7 +283,7 @@ static void duk_get_dbus_variant(duk_context* ctx, int idx, DBusMessageIter* ite
 static void duk_get_dbus_any(duk_context* ctx, int idx, DBusMessageIter* iter) {
 	const char* type = duk_get_field(ctx, idx, "type");
 
-	log_get(stderr, "[%p] get_any %s\n", iter, type);
+	log_get("[%p] get_any %s\n", iter, type);
 
 	if      (!strcmp(type, "ARRAY"))            duk_get_dbus_array(ctx, idx, iter);
 	else if (!strncmp(type, "DICT_ENTRY<", 11)) duk_get_dbus_dict_entry(ctx, idx, iter);
@@ -299,7 +299,7 @@ static void duk_get_dbus_array(duk_context* ctx, int idx, DBusMessageIter* iter)
 
 	duk_size_t len = duk_get_length(ctx, -1);
 
-	log_get(stderr, "[%p] get_array <%s> [%d]\n", iter, item_type, len);
+	log_get("[%p] get_array <%s> [%d]\n", iter, item_type, len);
 
 	const char* signature = dbus_get_signature(ctx, item_type);
 
@@ -318,7 +318,7 @@ static void duk_get_dbus_array(duk_context* ctx, int idx, DBusMessageIter* iter)
 static void duk_get_dbus_basic(duk_context* ctx, int idx, DBusMessageIter* iter) {
 	const char* basic_type = duk_get_field(ctx, idx, "type");
 
-	log_get(stderr, "[%p] get_basic %s\n", iter, basic_type);
+	log_get("[%p] get_basic %s\n", iter, basic_type);
 
 	int type;
 	DBusBasicValue value;
@@ -345,7 +345,7 @@ static void duk_get_dbus_basic(duk_context* ctx, int idx, DBusMessageIter* iter)
 		value.i64 = duk_get_number(ctx, -1);
 	} else if (!strcmp(basic_type, "OBJECT_PATH")) {
 		type = DBUS_TYPE_OBJECT_PATH;
-		value.str = (char*)duk_get_string(ctx, -1);
+		value.str = (char*)duk_get_char_pt(ctx, -1);
 	} else if (!strcmp(basic_type, "UINT16")) {
 		type = DBUS_TYPE_UINT16;
 		value.u16 = duk_get_number(ctx, -1);
@@ -357,10 +357,10 @@ static void duk_get_dbus_basic(duk_context* ctx, int idx, DBusMessageIter* iter)
 		value.u64 = duk_get_number(ctx, -1);
 	} else if (!strcmp(basic_type, "SIGNATURE")) {
 		type = DBUS_TYPE_SIGNATURE;
-		value.str = (char*)duk_get_string(ctx, -1);
+		value.str = (char*)duk_get_char_pt(ctx, -1);
 	} else if (!strcmp(basic_type, "STRING")) {
 		type = DBUS_TYPE_STRING;
-		value.str = (char*)duk_get_string(ctx, -1);
+		value.str = (char*)duk_get_char_pt(ctx, -1);
 	} else if (!strcmp(basic_type, "UNIX_FD")) {
 		type = DBUS_TYPE_UNIX_FD;
 		value.fd = duk_get_number(ctx, -1);
@@ -376,7 +376,7 @@ static void duk_get_dbus_basic(duk_context* ctx, int idx, DBusMessageIter* iter)
 }
 
 static void duk_get_dbus_dict_entry(duk_context* ctx, int idx, DBusMessageIter* iter) {
-	log_get(stderr, "[%p] get_dict_entry\n", iter);
+	log_get("[%p] get_dict_entry\n", iter);
 
 	DBusMessageIter sub;
 	dbus_message_iter_open_container(iter, DBUS_TYPE_DICT_ENTRY, NULL, &sub);
@@ -397,7 +397,7 @@ static void duk_get_dbus_struct(duk_context* ctx, int idx, DBusMessageIter* iter
 
 	duk_size_t len = duk_get_length(ctx, -1);
 
-	log_get(stderr, "[%p] get_struct [%d]\n", iter, len);
+	log_get("[%p] get_struct [%d]\n", iter, len);
 
 	DBusMessageIter sub;
 	dbus_message_iter_open_container(iter, DBUS_TYPE_STRUCT, NULL, &sub);
@@ -412,7 +412,7 @@ static void duk_get_dbus_struct(duk_context* ctx, int idx, DBusMessageIter* iter
 }
 
 static void duk_get_dbus_variant(duk_context* ctx, int idx, DBusMessageIter* iter) {
-	log_get(stderr, "[%p] get_variant\n", iter);
+	log_get("[%p] get_variant\n", iter);
 
 	duk_get_prop_string(ctx, idx, "value");
 
@@ -430,10 +430,10 @@ static void duk_get_dbus_variant(duk_context* ctx, int idx, DBusMessageIter* ite
 static duk_ret_t _js_call(duk_context* ctx) {
 	const int timeout = duk_get_int(ctx, 0);
 	DBusConnection* conn = duk_get_DBusConnection_pt(ctx, 1);
-	const char* destination = duk_get_string(ctx, 2);
-	const char* path = duk_get_string(ctx, 3);
-	const char* iface = duk_get_string(ctx, 4);
-	const char* method = duk_get_string(ctx, 5);
+	const char* destination = duk_get_char_pt(ctx, 2);
+	const char* path = duk_get_char_pt(ctx, 3);
+	const char* iface = duk_get_char_pt(ctx, 4);
+	const char* method = duk_get_char_pt(ctx, 5);
 	const int args_index = 6;
 
 	// Avoid NPEs
